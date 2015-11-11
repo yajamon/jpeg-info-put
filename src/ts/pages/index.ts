dd3f6977214511cf5aaa3adc0740ef7383909d14
yajamon <yajamon.tatsuki@gmail.com>
2015-11-11 22:51:13 +0900

    マーカー取得処理を作成

--- src/ts/pages/index.ts
+++ src/ts/pages/index.ts
@@ -36,9 +36,48 @@ window.addEventListener("load", function(){
     }, false);
 });
 
+enum JpegMark {
+    SOI = 0xffd8,
+    APP0 = 0xffe0,
+    APP1 = 0xffe1,
+    APP2 = 0xffe2,
+    APP3 = 0xffe3,
+    APP4 = 0xffe4,
+    APP5 = 0xffe5,
+    APP6 = 0xffe6,
+    APP7 = 0xffe7,
+    APP8 = 0xffe8,
+    APP9 = 0xffe9,
+    APP10 = 0xffea,
+    APP11 = 0xffeb,
+    APP12 = 0xffec,
+    APP13 = 0xffed,
+    APP14 = 0xffee,
+    APP15 = 0xffef,
+}
+
 function mainLoad(dataView:DataView) {
     var infos:any = [];
     var offset = 0;
 
+    var c=3
+    while (c>0) {    
+    // while (offset < dataView.byteLength) {
+        var marker = getMarker(dataView, offset);
+        var info:any = {};
+        info["marker"] = marker;
+        
+        infos.push(info);
+        --c;
+    }
     return infos;
+}
+
+function getMarker(dataView:DataView, offset:number) {
+    var byte = dataView.getUint8(offset);
+    if (byte != 0xff) {
+        return null;
+    }
+    var marker = <JpegMark>dataView.getUint16(offset);
+    return marker;
 }
