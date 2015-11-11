efa1dae77f2ee255f1254200e5bcb194360c7bc9
yajamon <yajamon.tatsuki@gmail.com>
2015-11-11 17:19:47 +0900

    画像を読みこませる

--- src/ts/pages/index.ts
+++ src/ts/pages/index.ts
@@ -1,3 +1,19 @@
+/// <reference path="../../../typings/es6-promise/es6-promise" />
+
 window.addEventListener("load", function(){
-    document.body.appendChild(document.createTextNode("Hello typescript world!"));
+    document.getElementById("jpgFile").addEventListener('change', (evt)=>{
+        var target =<HTMLInputElement>evt.target; // FileList object
+        var file = target.files[0];
+
+        var imgDrapPromise = new Promise((resolve)=>{
+            var reader = new FileReader();
+            reader.addEventListener("load", resolve);
+            reader.readAsDataURL(file);
+        }).then((loadEvent:ProgressEvent)=>{
+            var img = <HTMLImageElement>document.getElementById("imgView");
+            var reader = <FileReader>loadEvent.target;
+            img.src = reader.result;
+        });
+
+    }, false);
 });
